16f33d7910676639aab5db8fcf48d24e85ccfffa
ahmedakhtar11 <ahmedakhtar11@yahoo.com>
2020-04-12 00:40:36 -0500

    Starter Spring Template Upload

---
+++ src/main/java/com/example/GuestsJava/GuestListJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.GuestsJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestListJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestListJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GuestsJava/GuestListJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GuestsJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestListJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
